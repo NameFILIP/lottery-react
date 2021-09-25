453e3d9f736ff9c90244c99b7e0c584e0a18982d
Filip Spiridonov <filip.spiridonov@gmail.com>
2021-09-25 17:26:38 +0300

    Implement simple Lottery UI

---
+++ src/lottery.js
@@ -0,0 +1,47 @@
+import web3 from "./web3.js";
+
+const address = "0x6A01d3578a1A804f9510daE5Be1a8A8e3CEF09f3";
+const abi = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ internalType: "address[]", name: "", type: "address[]" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "manager",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+export default new web3.eth.Contract(abi, address);
